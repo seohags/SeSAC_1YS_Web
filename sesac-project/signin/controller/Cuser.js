4dc26db360433e72eee35dc089f8b36288ed4c93
금서하 <048965s@naver.com>
2022-12-10 19:06:11 +0900

    221210

--- sesac-project/signin/controller/Cuser.js
+++ sesac-project/signin/controller/Cuser.js
@@ -1,30 +1,27 @@
 const User = require("../model/User");
 const { connect } = require("../routes");
 
-exports.index = (req,res) => {
-    res.render("index");
+exports.index = (req, res) => {
+    res.render("index",);
 }
 
 exports.signin = (req, res) => {
-    res.render("signin");
+    res.render("signin",);
 }
 
 exports.signup = (req, res) => {
-    res.render("signup");
+    res.render("signup",);
 }
 
 exports.post_signup = (req, res) => {
     User.post_signup(req.body, () => {
-        // 여기에 회원가입을 하는 유효성 검사를 하면 좋은데 
         res.send(true);
     });
 }
-exports.post_signin = (req,res) => {    
-   User.post_signin(req.body.id , req.body.pw, (result) => {
-    // 여기도 .
-    if (result.length > 0) res.send(true);
-    else res.send(false);
-   });
+
+exports.post_signin = (req, res) => {
+    User.post_signin(req.body.id, req.body.pw, (result) => {
+        if (result.length > 0) res.send(true);
+        else res.send(false);
+    });
 }
-
-
